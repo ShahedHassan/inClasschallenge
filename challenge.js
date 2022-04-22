25ad2e1dba87c663a4d26354de4543e9cc2cdfb2
Shade Hassan <Shade@MacBook-Pro-4.local>
2022-04-22 12:52:28 -0500

    ugh

---
+++ challenge.js
@@ -0,0 +1,22 @@
+let oneArray = ["string1", "string2", "string3"];
+let secArray = [1, 2, 3];
+let thiArray = [(true)];
+
+if (
+function oneFunc(_oneArray[0]){
+    return (oneFunc == String));
+}
+console.log(oneArray.unshift("newstring"))
+
+else if ()
+function twoFunc(_secArray[0]){
+    return console.log(secArray.unshift(`4`));
+}
+console.log(secArray.unshift(`4`));
+
+else if(
+function thiFunc(_thiArray[0]){
+    return console.log(thiArray.unshift(`false`));
+}
+}
+console.log(thiArray.unshift(`False`));
