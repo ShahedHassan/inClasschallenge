95b41ff2d85f4448470a978a117dc2f88abb1f13
Shade Hassan <Shade@MacBook-Pro-4.local>
2022-04-22 13:00:44 -0500

    oof

--- challenge.js
+++ challenge.js
@@ -4,19 +4,23 @@ let thiArray = [(true)];
 
 if (
 function oneFunc(_oneArray){
-    return (oneFunc[0] == String));
-}
-console.log(oneArray.unshift("newstring"))
+    return (oneFunc[0] == String);
 
-else if ()
-function twoFunc(_secArray){
-    return (secArray[0] == Number));
+console.log(oneArray.unshift("newstring"))
 }
+);
+
+else if (
+function twoFunc(_secArray){
+    return (secArray[0] == Number);
+
 console.log(secArray.unshift(`4`));
+});
 
 else if(
 function thiFunc(_thiArray){
     return (thiArray[0] == Boolean);
-}
-}
-console.log(thiArray.unshift(`False`));
+
+
+console.log(thiArray.unshift(`False`);
+});
